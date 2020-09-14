d481af0d96212588506fc743e1772a905cb7d9f0
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-14 23:44:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  
+
+static capitalize (string) {
+ return string.charAt(0).toUpperCase() + string.slice(1);
+}
+static sanitize (string) {
+return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+}
+static titleize (string) {
+ let words = string.split(' ')
+  let wordsCap = words.map (word => {
+if (word !=='the' && word !=='a' && word !=='an' &&word !=='but' && word !=='of' && word !=='and'&& word !=='for' && word !=='at' && word !=='by' && word !=='from')
+return this.capitalize(word)
+else 
+return word;
+})
+return wordsCap.join(" ");
+}
+
 }
