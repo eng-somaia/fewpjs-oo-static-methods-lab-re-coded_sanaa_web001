4b33a31e96c1af540536a140c9dec0d7012270f8
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-14 23:51:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 class Formatter {
+  
+    static capitalize (string) {
+     return string.charAt(0).toUpperCase() + string.slice(1);
+    }
 
+    static sanitize (string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+    }
 
-static capitalize (string) {
- return string.charAt(0).toUpperCase() + string.slice(1);
-}
-static sanitize (string) {
-return string.replace(/[^A-Za-z0-9-' ]+/g, '');
-}
-static titleize (string) {
- let words = string.split(' ')
-  let wordsCap = words.map (word => {
-if (word !=='the' && word !=='a' && word !=='an' &&word !=='but' && word !=='of' && word !=='and'&& word !=='for' && word !=='at' && word !=='by' && word !=='from')
-return this.capitalize(word)
-else
-return word;
-})
-return wordsCap.join(" ");
-}
-
+    static titleize (string) {
+      let words = string.split(' ')
+      let wordsCap = words.map ((word, index) => {
+        if (word !=='the' && word !=='a' && word !=='an' &&word !=='but' && word !=='of' && word !=='and'&& word !=='for' && word !=='at' && word !=='by' && word !=='from')
+        return this.capitalize(word)
+        else if (index === 0)
+        return this.capitalize(word)
+        else 
+        return word;
+      });
+     return wordsCap.join(" ");
+    }
 }
